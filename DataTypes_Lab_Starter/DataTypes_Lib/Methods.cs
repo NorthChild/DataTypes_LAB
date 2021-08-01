357071c951c4194f372b6dc253ba05cff76def58
michael carini <snownkc@icloud.com>
2021-08-01 14:11:27 +0100

    dataTypes Lab #1 homework 95% done

---
+++ DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public static class Methods
+    {
+        // write a method to return the product of all numbers from 1 to n inclusive
+        public static int Factorial(int n)
+        {
+
+
+            int result = Methods.Factorial(n);
+            return result;
+
+            
+            //int result = 0;
+            //for (int i = n; i > 0; i--) 
+            //{
+            //    int factorial = n * i;
+            //    result += factorial;
+            //}
+           
+        }
+
+        public static float Mult(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ DataTypes_Lab_Starter/DataTypes_Lib/TypeConversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace DataTypes_Lib
+{
+    public class TypeConversion
+    {
+        public static short UIntToShort(uint num)
+        {
+
+            if (num == uint.MaxValue || num == uint.MinValue && num != 0)
+            {
+                throw new OverflowException();
+            }
+            else if (num == short.MaxValue + 1) 
+            {
+                throw new OverflowException();
+            }
+
+            return (short)num;
+        }
+
+        public static long FloatToLong(float num)
+        {
+            if (num == float.MaxValue || num == float.MinValue)
+            {
+                throw new OverflowException();
+            }
+            else if (num < 0) 
+            {
+                return (long)Math.Round(num);
+            }
+
+
+            return (long)Math.Round(num);
+        }
+    }
+}
